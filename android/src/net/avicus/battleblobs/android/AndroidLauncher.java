c49507969c0f125e03ab40b642f7ba4d2e84d9fc
Kylo <me@kylo.io>
2015-05-11 19:23:50 -0500

    LibGDX project.

---
+++ android/src/net/avicus/battleblobs/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.avicus.battleblobs.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.avicus.battleblobs.BattleBlobs;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BattleBlobs(), config);
+	}
+}
